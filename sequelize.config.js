fad26b97bcdbaa5a9011f3f5ddf6c95c2f66b70b
duypv <duypv98@gmail.com>
2020-10-05 00:48:58 +0700

    feat: setup sequelize with babel.

---
+++ sequelize.config.js
@@ -0,0 +1,15 @@
+require('@babel/register');
+require('./src/utils/dotenv').default.config();
+const API_NODE_ENV = process.env.API_NODE_ENV || 'local';
+
+module.exports = {
+  [API_NODE_ENV]: {
+    url: process.env.DB_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  },
+}
