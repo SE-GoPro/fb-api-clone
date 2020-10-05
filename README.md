5273244e6db9c13007f389001c2be99afb1f7e9a
duypv <duypv98@gmail.com>
2020-10-05 23:15:12 +0700

    feat: setup migrations + ORM

--- README.md
+++ README.md
@@ -15,6 +15,25 @@ HUST, SoICT, semester 20201
 - API Lists
 - [Response Codes](./docs/response-codes.md)
 
+## DB Scrips
+
+- Create a new migration: 
+  ```sh
+  npx sequelize migration:generate --name <migration_name>
+  ```
+- Run pending migrations
+  ```sh
+  npx sequelize db:migrate
+  ```
+- Revert a migration
+  ```sh
+  npx sequelize db:migrate:undo
+  ```
+- Revert all migrations ran
+  ```sh
+  npx sequelize db:migrate:undo:all
+  ```
+
 &copy; 2020 **SE Go Pro Team**
 > [Phùng Việt Duy](https://github.com/duypv98)
 > [Đào Duy Nam (Eric)](https://github.com/namdaoduy)
---
+++ migrations/20201005145427-create_tb_users.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      phone_number: Sequelize.STRING,
+      avatar_url: Sequelize.STRING,
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
