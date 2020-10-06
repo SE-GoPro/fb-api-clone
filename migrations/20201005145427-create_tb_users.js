9cc5b474d1280862b83e40f87ad5beff5d3f1629
duypv <duypv98@gmail.com>
2020-10-06 09:12:08 +0700

    feat: apply babel ES6 to migration file

--- migrations/20201005145427-create_tb_users.js
+++ migrations/20201005145427-create_tb_users.js
@@ -1,32 +1,27 @@
-'use strict';
-
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', {
-      id: {
-        type: Sequelize.BIGINT,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      password: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      phone_number: Sequelize.STRING,
-      avatar_url: Sequelize.STRING,
-    });
-  },
-
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('users');
-  }
-};
+export function up(queryInterface, Sequelize) {
+  return queryInterface.createTable('users', {
+    id: {
+      type: Sequelize.BIGINT,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    phone_number: Sequelize.STRING,
+    avatar_url: Sequelize.STRING,
+  });
+}
+export function down(queryInterface, Sequelize) {
+  return queryInterface.dropTable('users');
+}
