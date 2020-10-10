334ad507dc2239a76b506ce4bbd2784456c76e27
duypv98 <duypv98@gmail.com>
2020-10-10 11:16:38 +0700

    feat: add verifyToken middleware

---
+++ src/middlewares/verifyToken.js
@@ -0,0 +1,22 @@
+import { InvalidTokenError, UnauthorizedError } from 'common/errors';
+import jwt from 'jsonwebtoken';
+import Token from 'models/Token';
+
+export default async function verifyToken(req, res, next) {
+  try {
+    const bearerHeader = req.headers.authorization;
+    if (!bearerHeader) throw new UnauthorizedError();
+
+    const authHeaderParts = bearerHeader.split(' ');
+    if (authHeaderParts[0].toLowerCase() !== 'bearer' || !authHeaderParts[1]) throw new InvalidTokenError();
+
+    const token = await Token.findOne({ where: { token: authHeaderParts[1] } });
+    if (!token) throw new InvalidTokenError();
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, data) => {
+      if (err) throw new InvalidTokenError();
+      req.credentials = data;
+    });
+  } catch (e) {
+    next(e);
+  }
+}
