aef59883e730d63bcd763a4b9f69db65a215ff75
duypv98 <duypv98@gmail.com>
2020-10-10 11:18:51 +0700

    fix: code base #1

--- src/configs/base.js
+++ src/configs/base.js
@@ -1,5 +1,4 @@
 // Settings configured here will be merged into the final config object.
 export default {
   apiPort: 8000,
-  jwtSecret: process.env.TOKEN_SECRET,
 };
--- src/utils/handleResponses.js
+++ src/utils/handleResponses.js
@@ -1,6 +1,6 @@
 export default function handleResponse(res, data) {
   return res.status(200).json({
-    code: 1000,
+    code: '1000',
     message: 'OK',
     data: data || null,
   });
