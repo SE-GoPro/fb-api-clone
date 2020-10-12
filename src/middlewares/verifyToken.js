ab2e4eb9f8aaee5d8fe9eab94702596b317c191b
duypv98 <duypv98@gmail.com>
2020-10-12 12:23:12 +0700

    refactor: verifyToken with token from body

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -4,19 +4,24 @@ import Token from 'models/Token';
 
 export default async function verifyToken(req, res, next) {
   try {
-    const bearerHeader = req.headers.authorization;
-    if (!bearerHeader) throw new UnauthorizedError();
+    // const bearerHeader = req.headers.authorization;
+    // if (!bearerHeader) throw new UnauthorizedError();
 
-    const authHeaderParts = bearerHeader.split(' ');
-    if (authHeaderParts[0].toLowerCase() !== 'bearer' || !authHeaderParts[1]) throw new InvalidTokenError();
+    // const authHeaderParts = bearerHeader.split(' ');
+    // if (authHeaderParts[0].toLowerCase() !== 'bearer'
+    //   || !authHeaderParts[1]
+    // ) throw new InvalidTokenError();
 
-    const token = await Token.findOne({ where: { token: authHeaderParts[1] } });
+    // const token = await Token.findOne({ where: { token: authHeaderParts[1] } });
+    if (!req.body.token) throw new UnauthorizedError();
+    const token = await Token.findOne({ where: { token: req.body.token } });
     if (!token) throw new InvalidTokenError();
     jwt.verify(token, process.env.TOKEN_SECRET, (err, data) => {
       if (err) throw new InvalidTokenError();
       req.credentials = data;
     });
+    return next();
   } catch (e) {
-    next(e);
+    return next(e);
   }
 }
